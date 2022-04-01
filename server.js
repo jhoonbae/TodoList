897ed3f595a8afc726c2324e04b636306db3f9d9
jhoonbae <bae156@naver.com>
2022-04-02 01:49:42 +0900

    second commit

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const app = express();
 const port = 4833;
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect('mongodb+srv://jhoon:wjdgns12@cluster0.whpog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',(err,client)=>{
-    if(err){return console.log("에러에러에러")}
+    if(err){return console.log(err)}
+
+    db = client.db('todolist'); // db 지정
+
     app.listen(port,()=>{
         console.log("listening on",port);
     });
 })
 
 app.use(express.urlencoded({extended: true}));
-
-
+app.set('view engine', 'ejs');
 
 app.get("/",(req,res)=>{ // main page
     res.sendFile(__dirname+"/index.html");
@@ -23,7 +25,23 @@ app.get("/w",(req,res)=>{ // 글작성 페이지
 
 app.post("/add", (req,res)=>{ // 글작성 요청
     res.send("전송 완료");
-    console.log(req.body)
+    console.log(req.body.title);
+    console.log(req.body.date);
+
+    var title = req.body.title;
+    var date = req.body.date;
+
+    // hoon collection에 데이터 삽입
+    db.collection('hoon').insertOne({제목 : title, 날짜 : date}, (err,res)=>{ // db 저장 형식
+        console.log('저장완료');
+    });
 });
 
-
+app.get('/list',(req,res)=>{
+    // find = select * , toarray로 원하는 형식으로 가져온다.
+    db.collection('hoon').find().toArray((err,result)=>{
+        // console.log(result);
+        res.render('list.ejs', {result : result}); // ejs에 넘겨줄 결과값 지정
+    });
+    
+});
