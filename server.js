c2e9188ce581948add3562d588caf46c2e3f82f3
jhoonbae <bae156@naver.com>
2022-04-03 19:21:17 +0900

    sunday

--- server.js
+++ server.js
@@ -12,30 +12,33 @@ MongoClient.connect('mongodb+srv://jhoon:wjdgns12@cluster0.whpog.mongodb.net/myF
     });
 })
 
+app.use('/public', express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get("/",(req,res)=>{ // main page
-    res.sendFile(__dirname+"/index.html");
+    res.render("index.ejs");
 });
 
 app.get("/w",(req,res)=>{ // 글작성 페이지
-    res.sendFile(__dirname+"/write.html");
+    res.render("write.ejs");
 });
 
 app.post("/add", (req,res)=>{ // 글작성 요청
-    res.send("전송 완료");
-    console.log(req.body.title);
-    console.log(req.body.date);
-
     var title = req.body.title;
-    var date = req.body.date;
-
-    // hoon collection에 데이터 삽입
-    db.collection('hoon').insertOne({제목 : title, 날짜 : date}, (err,res)=>{ // db 저장 형식
-        console.log('저장완료');
+    var date = req.body.date; 
+    db.collection('counter').findOne({name : '게시물갯수'}, (err, result)=>{
+        var total = result.totalPost;
+            // hoon collection에 데이터 삽입
+            db.collection('hoon').insertOne({_id : total + 1, 제목 : title, 날짜 : date}, (err,result)=>{ // db 저장 형식
+                // counter collection에 tost개수 auto increase
+                db.collection('counter').updateOne({name:'게시물갯수'},{ $inc: {totalPost:1} },function(err, result){
+                    if(err){return console.log(err)}
+                    res.render('write.ejs');
+                });
+            });
+        });
     });
-});
 
 app.get('/list',(req,res)=>{
     // find = select * , toarray로 원하는 형식으로 가져온다.
@@ -45,3 +48,31 @@ app.get('/list',(req,res)=>{
     });
     
 });
+
+app.delete('/delete', (req,res)=>{
+    console.log(req.body);
+    targetId = parseInt(req.body._id);
+    db.collection('counter').findOne({name : '게시물갯수'}, (err, result)=>{
+        var total = result.totalPost;
+        db.collection('hoon').deleteOne({_id : targetId}, (err,result)=>{
+            db.collection('counter').updateOne({name:'게시물갯수'},{$inc:{totalPost:-1}}, (err,result)=>{
+                if(!err){
+                    console.log("삭제완료")
+                    res.status(200).send({ message : '성공했습니다.' });
+                }
+            });
+        });
+    });
+});
+
+// : => 뒤에 오는 문자열에 맞게 응답.
+app.get('/detail/:id', (req,res)=>{
+    db.collection('hoon').findOne({_id : parseInt(req.params.id)},(err, result)=>{
+        if(err){
+            res.send("페이지 오류입니다")
+        }else{
+            console.log(result);
+            res.render('detail.ejs', {data : result});
+        }
+    })
+})
